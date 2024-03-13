962350ee61fca53ca7ec85af137b9144414e7838
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-03-13 00:28:29 -0300

    Terminado Componente NavBar
    Bootstrap generó muchos problemas al momento de querer centrar
    los elementos del componente, pero después de muchas modificaciones
    se logró el objetivo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,32 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-
-import CartWidget from "./CartWidget"
+import Navbar from "react-bootstrap/Navbar";
+import CartWidget from "./CartWidget";
 
 function NavBar() {
   return (
     <>
-      <Navbar expand="lg" bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <div className="container d-flex justify-content-between align-items-center">
+          <Navbar.Brand href="#" className="me-0">
             <h2>React Store</h2>
           </Navbar.Brand>
+
+          <div className="navbar_link-container">
+            <a href="#">
+              <h5>Notebooks</h5>
+            </a>
+            <a href="#">
+              <h5>Teléfonos</h5>
+            </a>
+            <a href="#">
+              <h5>Componentes</h5>
+            </a>
+          </div>
+
           <CartWidget />
-        </Container>
+        </div>
       </Navbar>
     </>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
