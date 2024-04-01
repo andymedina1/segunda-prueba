75ad1976fcdc9ae5b14267350d4eba19914da7a5
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-04-01 05:30:58 -0300

    Creación plantilla objeto mock y desarrollo de ItemListContainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,5 +1,28 @@
-function ItemListContainer ({ greeting }) {
-  return <h1 className='text-center mt-5'>{greeting}</h1>
+import { useState, useEffect } from 'react'
+
+import data from '../data/products.json'
+
+function ItemListContainer () {
+  const [products, setProducts] = useState([])
+
+  // Efecto secundario para obtener los datos
+  useEffect(() => {
+    const promesa = new Promise((resolve) => {
+      // Simular retraso de red
+      setTimeout(() => resolve(data), 2000)
+    })
+
+    promesa.then((data) => { setProducts(data) })
+  }, [])
+
+  return (
+    <>
+      {products.map(product =>
+        <h1 className='text-center mt-5' key={product.id}>
+          {product.title}
+        </h1>)}
+    </>
+  )
 }
 
 export default ItemListContainer
