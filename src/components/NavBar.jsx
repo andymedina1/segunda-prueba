0b29c384f4124f7ff053e63c25117e4b7f27a770
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-04-01 05:01:36 -0300

    Instalación de Standard y formateo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,23 @@
-import Navbar from "react-bootstrap/Navbar";
-import CartWidget from "./CartWidget";
+import Navbar from 'react-bootstrap/Navbar'
+import CartWidget from './CartWidget'
 
-function NavBar() {
+function NavBar () {
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <div className="container d-flex justify-content-between align-items-center">
-          <Navbar.Brand href="#" className="me-0">
+      <Navbar bg='dark' data-bs-theme='dark'>
+        <div className='container d-flex justify-content-between align-items-center'>
+          <Navbar.Brand href='#' className='me-0'>
             <h2>React Store</h2>
           </Navbar.Brand>
 
-          <div className="navbar_link-container">
-            <a href="#">
+          <div className='navbar_link-container'>
+            <a href='#'>
               <h5>Notebooks</h5>
             </a>
-            <a href="#">
+            <a href='#'>
               <h5>Teléfonos</h5>
             </a>
-            <a href="#">
+            <a href='#'>
               <h5>Componentes</h5>
             </a>
           </div>
@@ -26,7 +26,7 @@ function NavBar() {
         </div>
       </Navbar>
     </>
-  );
+  )
 }
 
-export default NavBar;
+export default NavBar
