63ce9c3f176133d9747b955939a8fdbd4a764693
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-04-02 02:25:17 -0300

    Implementada pantalla de carga entre rutas

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -6,12 +6,14 @@ import ItemList from './ItemList'
 
 function ItemListContainer () {
   const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
   const { id } = useParams()
 
   // Efecto secundario para obtener los datos
   useEffect(() => {
     const promesa = new Promise((resolve) => {
       // Simular retraso de red
+      setLoading(true)
       setTimeout(() => resolve(data), 2000)
     })
 
@@ -19,17 +21,21 @@ function ItemListContainer () {
       if (!id) {
         // Si no hay un id en la ruta, muestro todos los productos
         setProducts(data)
+        setLoading(false)
       } else {
         // Si la ruta tiene un id, filtro los productos por categoría
         const productosFiltrados = data.filter((prod) => prod.category === id)
         setProducts(productosFiltrados)
+        setLoading(false)
       }
     })
   }, [id])
 
   return (
     <>
-      <ItemList products={products} />
+      {loading
+        ? <h1 className='text-center m-5'>Loading...</h1>
+        : <ItemList products={products} />}
     </>
   )
 }
