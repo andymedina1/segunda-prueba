88bbbcd7e25dbc25d96174273bc32adbb26c0e45
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-04-02 01:32:39 -0300

    Añadida función para navegar a 'item/:id'
    Respetando la consigna, ahora cada vez que se haga click sobre un item,
    la página mostrará los detalles del mismo

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,19 @@
 import Card from 'react-bootstrap/Card'
+import { NavLink } from 'react-router-dom'
 
-function Item ({ title, description, price, pictureUrl }) {
+function Item ({ id, title, description, price, pictureUrl }) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant='top' src={pictureUrl} alt={'Imágen de ' + title} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>
-          {description} {price}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <NavLink to={'/item/' + id}>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant='top' src={pictureUrl} alt={'Imágen de ' + title} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+            {description} {price}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </NavLink>
   )
 }
 
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -2,15 +2,17 @@ import Card from 'react-bootstrap/Card'
 
 function ItemDetail ({ title, description, price, pictureUrl }) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant='top' src={pictureUrl} alt={'Imágen de ' + title} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>
-          {description} {price}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <div className='d-flex justify-content-center mt-5'>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant='top' src={pictureUrl} alt={'Imágen de ' + title} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>
+            {description} {price}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
 
