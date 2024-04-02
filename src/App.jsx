2ee22e2c9f7b7ca1e37902fc78bccac8cd275b5a
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-04-02 15:33:07 -0300

    Arreglado bug en nombre del param

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App () {
         <NavBar />
         <Routes>
           <Route path='/' element={<ItemListContainer />} />
-          <Route path='/category/:id' element={<ItemListContainer />} />
+          <Route path='/category/:category' element={<ItemListContainer />} />
           <Route path='/item/:id' element={<ItemDetailContainer />} />
         </Routes>
       </BrowserRouter>
