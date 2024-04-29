3dc21b2f24b8edab6b4a7d6436ad9626b1baf61c
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-04-29 16:27:16 -0300

    Act.6 - Primer prueba de Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,21 @@
+import { useContext } from 'react'
+import { CartContext } from '../contexts/CartContext'
+
 function Cart () {
-  return (<>Hola mundo!</>)
+  const { cartList } = useContext(CartContext)
+  return (
+    <div className='mt-5 ms-5'>
+      <h1>Prueba carrito</h1>
+      <div className='mt-5'>
+        {cartList.map(item =>
+          <ul key={item.id}>
+            <h6>{item.title}</h6>
+            <li>Precio unitario: {item.price}</li>
+            <li>Cantidad: {item.quantity} unidades</li>
+          </ul>)}
+      </div>
+    </div>
+  )
 }
 
 export default Cart
