b17e86ded699d7f902a0394defa50ff71437511e
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-05-03 00:02:14 -0300

    Act.7 - Conectado Firestore en ItemListContainer
    Se eliminaron los mocks de peticiones y se implementó el uso de
    'getDocs' de Firebase para obtener los datos desde Firestore.
    
    También se cambiaron los 'id' de los productos; ahora se usan
    los que genera Firestore.

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,30 +1,38 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
-import data from '../data/products.json'
 import ItemList from './ItemList'
+
+import { app } from '../App'
+import { collection, getDocs, getFirestore } from 'firebase/firestore'
 
 function ItemListContainer () {
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState(true)
   const { category } = useParams()
 
-  useEffect(() => {
-    const promesa = new Promise((resolve) => {
-      setLoading(true)
-      setTimeout(() => resolve(data), 2000)
-    })
+  const db = getFirestore(app)
+  const refCollection = collection(db, 'items')
 
-    promesa.then((data) => {
-      if (!category) {
-        setProducts(data)
-        setLoading(false)
-      } else {
-        const productosFiltrados = data.filter((prod) => prod.category === category)
-        setProducts(productosFiltrados)
-        setLoading(false)
-      }
-    })
+  useEffect(() => {
+    let data
+    getDocs(refCollection)
+      .then((snapshot) => {
+        data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        return data
+      })
+      .then((data) => {
+        if (!category) {
+          setProducts(data)
+          setLoading(false)
+        } else {
+          const productosFiltrados = data.filter(
+            (prod) => prod.category === category
+          )
+          setProducts(productosFiltrados)
+          setLoading(false)
+        }
+      })
   }, [category])
 
   return (
