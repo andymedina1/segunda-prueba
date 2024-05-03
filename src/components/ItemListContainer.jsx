6317b1760800961133d0fc5543e5b576f198eec3
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-05-03 11:06:33 -0300

    Act.7 - Consulta con query filtrando categorías

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -4,33 +4,36 @@ import { useParams } from 'react-router-dom'
 import ItemList from './ItemList'
 
 import db from '../services/firebase'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, query, where } from 'firebase/firestore'
 
 function ItemListContainer () {
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState(true)
   const { category } = useParams()
 
-  const refCollection = collection(db, 'items')
-
   useEffect(() => {
-    let data
-    getDocs(refCollection)
+    setLoading(true)
+
+    let q
+    if (!category) {
+      q = query(collection(db, 'items'))
+    } else {
+      q = query(collection(db, 'items'), where('category', '==', category))
+    }
+
+    getDocs(q)
       .then((snapshot) => {
-        data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data()
+        }))
         return data
       })
       .then((data) => {
-        if (!category) {
-          setProducts(data)
-          setLoading(false)
-        } else {
-          const productosFiltrados = data.filter(
-            (prod) => prod.category === category
-          )
-          setProducts(productosFiltrados)
-          setLoading(false)
-        }
+        setProducts(data)
+      })
+      .finally(() => {
+        setLoading(false)
       })
   }, [category])
 
