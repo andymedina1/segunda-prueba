7734ad91e0e44fdeadfc65f22cb069892950afbd
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-05-04 10:48:11 -0300

    Eliminado no-mostrar widget con carrito vacío

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -10,7 +10,6 @@ function CartWidget () {
     <>
       <Link
         to='/cart'
-        style={{ visibility: totalCartQuantity() > 0 ? 'visible' : 'hidden' }}
       >
         <svg
           xmlns='http://www.w3.org/2000/svg'
